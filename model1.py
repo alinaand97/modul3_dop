88b4f94d3e50d44dbd3f21827ac87e2b3053371a
alinaand97 <alinaand_97@mail.ru>
2024-07-07 16:03:24 +1000

    Add files via upload

---
+++ model1.py
@@ -0,0 +1,30 @@
+# Модули и пакеты
+data = [
+  [1, 2, 3],
+  {'a': 4, 'b': 5},
+  (6, {'cube': 7, 'drum': 8}),
+  "Hello",
+  ((), [{(2, 'Urban', ('Urban2', 35))}])
+]
+
+def calculate_structure_sum(data, sums=0):
+    if isinstance(data, str):
+        return len(data)
+
+    elif isinstance(data, (int, float)):
+        return data
+
+    elif isinstance(data, (list, tuple, set)):
+        for item in data:
+            sums += calculate_structure_sum(item)
+
+    elif isinstance(data, dict):
+        for key, value in data.items():
+            sums += calculate_structure_sum(key)
+            sums += calculate_structure_sum(value)
+
+    return sums
+
+
+result = calculate_structure_sum(data)
+print(result)
